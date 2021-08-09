c9a16fc171aaf5f9d2e8c71194b53d2f3e6dcf0a
Filipe Lopes <filiperocklopes@gmail.com>
2021-08-09 09:07:09 -0300

    update deploy script

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
-cd reactapp
-git checkout release
-git pull
-cd ..
 git checkout develop
 git pull
+git submodule update
 docker-compose -f docker-compose.prod.yml down
 docker-compose -f docker-compose.prod.yml build --no-cache
 docker-compose -f docker-compose.prod.yml up -d
